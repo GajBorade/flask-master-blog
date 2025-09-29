74feb81ec09a93bb1939adb5433a5c88528a89dd
GajBorade <141010355+GajBorade@users.noreply.github.com>
2025-09-29 15:02:21 +0200

    Set up Flask routes and read blog posts from JSON

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+import json
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return ("Hello, World! 🎉 Try visiting /index, /add,"
+            "/update, /delete")
 
 
-if __name__ == '__main__':
+@app.route("/index")
+def index():
+    # add code here to fetch the job posts from a file
+    with open("data/blog.json", "r", encoding="utf-8") as file_object:
+        blog_posts = json.load(file_object)
+    print(blog_posts)  # list of dict
+    return render_template("index.html", posts=blog_posts)
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
